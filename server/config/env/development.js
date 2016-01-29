176f2e1688123c1c4e13b2d2a16895001a4babe0
laurent <laurent.tempier@gmail.com>
2016-01-29 13:47:11 +0100

    change configs

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -4,12 +4,9 @@ module.exports = {
     env: 'development',
     ip: '0.0.0.0',
     port: 1989,
-    mongo: {
-        url: 'mongodb://toor:2HLV3g2jNiB252o@ds045475.mongolab.com:45475/matrix'
-    },
     firebase: {
-        url: 'https://matrixdev.firebaseio.com/',
-        secret: 'gIyaDWGE9aj8VeNJRfWrgcDwI0FJjcMmxaEh8ZIk',
+        url: 'https://matrixled.firebaseio.com/',
+        secret: '2XFZUirWPGqLVuuCQTngHpVVFZXNa6N8UOlA3eJZ',
         uid: 'server'
     },
     mqtt: {
--- server/config/env/production.js
+++ server/config/env/production.js
@@ -10,11 +10,20 @@ module.exports = {
         uid: 'server'
     },
     mqtt: {
-        url: 'mqtt://toor:2HLV3g2jNiB252o@m20.cloudmqtt.com:19793',
-        port: 19793,
-        hostname: 'm20.cloudmqtt.com',
-        username: 'toor',
-        password: '2HLV3g2jNiB252o',
-        protocol: 'mqtt'
+        port: 1883,
+        hostname: '195.154.118.152',
+        //username: 'toor',
+        //password: '2HLV3g2jNiB252o',
+        protocol: 'mqtt',
+        protocolId: 'MQIsdp',
+        protocolVersion: 3
     }
+    //mqtt: {
+    //    url: 'mqtt://toor:2HLV3g2jNiB252o@m20.cloudmqtt.com:19793',
+    //    port: 19793,
+    //    hostname: 'm20.cloudmqtt.com',
+    //    username: 'toor',
+    //    password: '2HLV3g2jNiB252o',
+    //    protocol: 'mqtt'
+    //}
 };
